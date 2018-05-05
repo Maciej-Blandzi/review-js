c52b5dcf432d91ddced0ae1544ae320735c37c68
maciabe <maciabe@hotmail.com>
2018-05-05 03:58:33 +0200

    'js review'

---
+++ table.js
@@ -0,0 +1,32 @@
+
+
+function createTable(width,height)
+{
+
+    var table = document.createElement('table')
+    var tbody = document.createElement('tbody')
+    macia.appendChild(table)
+    table.appendChild(tbody)
+
+
+    var tr, td;
+    for (var i = 0; i < height; i += 1) {
+        tr = document.createElement('tr')
+        tbody.appendChild(tr)
+        for (var j = 0; j < width; j += 1) {
+            td = document.createElement('td')
+            tr.appendChild(td)
+        }
+    }return table
+}
+
+
+function klikniecie(klik){
+    if(klik.target.matches('td')){
+        var intervalId = setInterval(function(){
+        if(klik.target.classList.contains('red')){
+            klik.target.classList.remove('red')
+        }else{klik.target.classList.add('red')}
+    },700)}
+    setTimeout(function(){clearInterval(intervalId)},10000)
+}
