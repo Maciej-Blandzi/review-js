54479e3329c742b0709c1df1110d745862fd0071
maciabe <maciabe@hotmail.com>
2018-05-05 18:43:16 +0200

    'page scroll pt1 + filling-up animation'

---
+++ scroll.js
@@ -0,0 +1,11 @@
+var link = document.querySelectorAll('#menu a')[1];
+var menu = document.querySelector('#menu')
+
+link.addEventListener('click', function(event){
+    event.preventDefault()
+
+console.log(menu.offsetHeight)
+
+    var targetElement = document.querySelector('#section2')
+    
+})
