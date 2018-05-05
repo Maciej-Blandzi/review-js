f69edfbe5444a3a7676064c71b6bd9dd84cf3159
maciabe <maciabe@hotmail.com>
2018-05-05 21:10:54 +0200

    'delete btns'

--- form.js
+++ form.js
@@ -1,26 +1,50 @@
 
 
-var form = document.querySelector('form')
+var form = document.querySelector('form');
 
 var input = document.querySelector('input');
-var ul = document.getElementById('notes-list')
+var textarea = document.querySelector('textarea');
+var ul = document.getElementById('notes-list');
 
-var message = document.getElementById('info')
+var message = document.getElementById('info');
 
 
 form.addEventListener('submit', function(event){
-    event.preventDefault()
+    event.preventDefault();
+    var li = document.createElement('li');
 
-    var li = document.createElement('li')
-    ul.appendChild(li)
-    li.textContent = input.value
-})
+    var header = document.createElement('h2')
+    var description = document.createElement('p')
+    var deleteButton = document.createElement('button')
+
+    deleteButton.addEventListener('click', function(event){
+        event.currentTarget.parentElement.remove()
+    })
+
+    header.textContent = input.value;
+    description.textContent = textarea.value;
+    deleteButton.textContent = 'DELETE ENTRY';
+
+    li.appendChild(header)
+    li.appendChild(description)
+    li.appendChild(deleteButton)
 
 
 
-input.addEventListener('blur',function(x){
+    // li.textContent = header + description
+
+
+
+    input.value = '';
+    textarea.value = '';
+    ul.appendChild(li);
+});
+
+
+
+input.addEventListener('blur',function(){
     if(input.value.length < 5){
 message.textContent = '5 digits minimum'
     }else{message.textContent = ''}
 
-})
+});
